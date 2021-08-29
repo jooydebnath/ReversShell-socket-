4fd4744b61811cc10a8aa2839cf5e2cfdf3920e5
Joy Debnath <joy91740@gmail.com>
2021-08-29 23:45:26 +0600

    server.py create two funtion frist (Create a Socket) secound (Binding the socket and listening for connections)

---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+
+
+#Create a Socket (connect two computer)
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9090
+        s = socket.socket()
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+# Binding the socket and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print("Binding the Port" + str(port))
+
+        s.bind((host,port))
+        s.listen(5)
+
+    except socket.error as msg:
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying.....")
+        bind_socket()
